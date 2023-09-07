d77d9fe5a27079472371476a3950b4caf84ef336
xuchengyun <xuchengyun@163.com>
2023-09-07 15:11:21 +0800

    dd

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+
+const gulp = require('gulp')
+const babel = require('gulp-babel')
+const del = require('del')
+var uglify = require('gulp-uglify')
+
+function clean(cb) {
+  console.log(111)
+  del.sync('lib')
+  cb()
+}
+
+function build() {
+  return gulp.src('src/**/*.js')
+  .pipe(
+    babel(
+      {presets: ['@babel/env']}
+    )
+  ).pipe(
+    uglify({
+      mangle: true,
+      compress: true
+    })
+  ).pipe(gulp.dest('lib'))
+
+  
+  // return gulp.src('src/**/*.js')
+  // .pipe(
+  //   babel(
+  //     {presets: ['@babel/env']}
+  //   )
+  // ).pipe(gulp.dest('lib'))
+}
+
+gulp.src('src/**/*.js')
+  .pipe(
+    babel(
+      {presets: ['@babel/env']}
+    )
+  ).pipe(
+    uglify({
+      mangle: true,
+      compress: true
+    })
+  ).pipe(gulp.dest('lib'))
+
+module.exports = gulp.series(clean,build)
---
+++ src/bin.js
@@ -0,0 +1,22 @@
+var commander = require('commander');
+var whistle = require('./whistle/index');
+var autoEditVersion = require('./autoEditVersion/index');
+const pkg = require("../package.json")
+
+
+const service = new commander.Command('service').description('启动项目代理');
+
+
+service.version(pkg.version,'-v, --version')
+service.command("proxy").description('启动项目代理')
+    .action((name, options) => {
+        whistle()
+    })
+
+service.command("autoVersion").description('自动与Master版本对比，取最大版本，并自动更新版本')
+    .action((name, options) => {
+        new autoEditVersion()
+})
+
+
+service.parse(process.argv)
