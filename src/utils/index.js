2160abd70a931b1e3d5cccace7cf50d8365a202a
xuchengyun <xuchengyun@163.com>
2023-09-07 11:11:28 +0800

    dd

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,19 +1,19 @@
 const chalk = require("chalk");
 const log = {
-    normal: (msg)=>{
+    normal: (msg) => {
         console.log(msg)
     },
     error: (msg) => {
         console.log(chalk.red(msg))
     },
-    success:(msg) => {
+    success: (msg) => {
         console.log(chalk.green(msg))
     },
-    warn:(msg) => {
+    warn: (msg) => {
         console.yellow(chalk.green(msg))
-    },
+    }
 
-
+}
 
 module.exports = {
     log
