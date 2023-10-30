72c32fcbdc02b26929d5787f9dee3a8963320f0d
mengxuhui <i.mengxh@gmail.com>
2023-10-30 14:15:37 +0800

    feat: build

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 type Props = {}
 
-export default function index({}: Props) {
+export default function index({ }: Props) {
   return (
     <div>index</div>
   )
 }
 
-//ci 1.0.21
+//ci 1.0.22
