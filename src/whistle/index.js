45006c632cb2d4bf2b074eb12cffa5bc3ff56bf4
zhangsirui <zhangsirui@beisen.com>
2023-10-31 16:43:26 +0800

    feat: 动态path和port

--- src/whistle/index.js
+++ src/whistle/index.js
@@ -1,4 +1,5 @@
 const fileName = '.proxy.whistle.js'
+const webpackConfigName = 'webpack.config.js'
 const fs = require('fs')
 const path = require('path')
 const cli = require('cac')()
@@ -7,12 +8,36 @@ const inquirer = require('inquirer')
 const template = require('art-template')
 const shell = require('shelljs')
 const configSrc = path.join(process.cwd(), `./${fileName}`)
+const webpackSrc = path.join(process.cwd(), `./${webpackConfigName}`)
+const webpackConfig = require(webpackSrc)
 const { compareVersions } = require('compare-versions')
 const { log } = require('../utils')
+console.log(
+  'pathpath',
+  process.cwd(),
+  path.sep,
+  'configSrcconfigSrc',
+  webpackSrc
+)
+
+// const path1 = webpackConfig.output.publicPath
+// const port1 = webpackConfig.port
+// const projectName = path1.slice(path1.match(/ux/).index + 2, path1.length)
+// console.log('asjbjhbasd', path1, projectName, port1)
 
 function getProjectName() {
-  const pPath = process.cwd()
-  return pPath.substring(pPath.lastIndexOf(path.sep) + 1)
+  //   const pPath = process.cwd()
+  //   return pPath.substring(pPath.lastIndexOf(path.sep) + 1)
+  const pPath = webpackConfig.output?.publicPath
+  if (pPath) {
+    const projectNamePath = pPath.slice(
+      pPath.match(/ux/).index + 2,
+      pPath.length
+    )
+    return projectNamePath
+  } else {
+    log.error(`请检查webpack.config.js，找不到正确path`)
+  }
 }
 
 function openProxy() {
@@ -37,12 +62,21 @@ function tempalte() {
   return `const pkg = require('./package.json');
     exports.groupName = '项目开发环境'; // 可选，设置分组， 要求 Whistle 版本 >= v2.9.21
     exports.name = "{{data.projectName}}";
-    exports.rules = ${a}/{{data.projectName}}/release/dist/(.*)(?:-).*(.bundle|.chunk)?(?:.min)(..*)$/  127.0.0.1:{{data.port}}/$1$3
-/{{data.projectName}}/release/dist/(.*)(?:.min)(..*)$/  127.0.0.1:{{data.port}}/$1$2
-/{{data.projectName}}/release/dist/images/(.*)/ 127.0.0.1:{{data.port}}/images/$1
-/{{data.projectName}}/release/dist/(.*)/ 127.0.0.1:{{data.port}}/$1
+    exports.rules = ${a}{{data.projectName}}(.*)(?:-).*(.bundle|.chunk)?(?:.min)(..*)$/  127.0.0.1:{{data.port}}/$1$3
+{{data.projectName}}(.*)(?:.min)(..*)$/  127.0.0.1:{{data.port}}/$1$2
+{{data.projectName}}images/(.*)/ 127.0.0.1:{{data.port}}/images/$1
+{{data.projectName}}(.*)/ 127.0.0.1:{{data.port}}/$1
     ${a}
    `
+  //   return `const pkg = require('./package.json');
+  //     exports.groupName = '项目开发环境'; // 可选，设置分组， 要求 Whistle 版本 >= v2.9.21
+  //     exports.name = "{{data.projectName}}";
+  //     exports.rules = ${a}/{{data.projectName}}/release/dist/(.*)(?:-).*(.bundle|.chunk)?(?:.min)(..*)$/  127.0.0.1:{{data.port}}/$1$3
+  // /{{data.projectName}}/release/dist/(.*)(?:.min)(..*)$/  127.0.0.1:{{data.port}}/$1$2
+  // /{{data.projectName}}/release/dist/images/(.*)/ 127.0.0.1:{{data.port}}/images/$1
+  // /{{data.projectName}}/release/dist/(.*)/ 127.0.0.1:{{data.port}}/$1
+  //     ${a}
+  //    `
 }
 async function createConfig(port) {
   let projectName = getProjectName()
@@ -85,12 +119,13 @@ async function createFile() {
   const { type } = answers
   if (!type) return
 
+  const { port = 3000 } = webpackConfig
   const answersPort = await inquirer.prompt([
     {
       type: 'input',
       name: 'port',
-      message: `输入port，默认3000`,
-      default: '3000',
+      message: `输入port，默认${port}`,
+      default: port,
     },
   ])
   await createConfig(answersPort.port)
